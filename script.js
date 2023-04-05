b9c0da25a0b403d371186774d1b75d1b92be79a0
nouman-munib <thesmartlad27@gmail.com>
2023-04-05 20:11:55 +0500

    remove item feature added

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const addItems = document.querySelector(".add-items");
+const removeItems = document.querySelector(".remove-items");
+const clearAllBtn = document.querySelector(".clear-all");
 const itemsList = document.querySelector(".plates");
+
 const items = JSON.parse(localStorage.getItem("items")) || [];
 
 const addItem = function (e) {
@@ -8,7 +11,7 @@ const addItem = function (e) {
   const text = this.querySelector('[name="item"]').value;
   const item = {
     text,
-    done: false,
+    done: false, // NOT checked initially
   };
   items.push(item);
   displayList(items, itemsList);
@@ -45,7 +48,39 @@ const toggleChecked = function (e) {
   displayList(items, itemsList);
 };
 
+const removeItem = function (e) {
+  e.preventDefault();
+
+  const checkedItems = items.filter((checkedItem) => checkedItem.done === true);
+  checkedItems.forEach((checkedItem) => {
+    const index = items.indexOf(checkedItem);
+    items.splice(index, 1);
+  });
+
+  localStorage.setItem("items", JSON.stringify(items));
+  displayList(items, itemsList);
+};
+
+const clearAll = function (e) {
+  e.preventDefault();
+
+  items.splice(0, items.length);
+
+  localStorage.setItem("items", JSON.stringify(items));
+  displayList(items, itemsList);
+};
+
+// Another method to clear the items list:
+// const clearAll = function (e) {
+//   items.length = 0; // set the items array to empty
+
+//   localStorage.removeItem("items"); // remove the items from localStorage
+//   displayList(items, itemsList);
+// };
+
 displayList(items, itemsList);
 
 addItems.addEventListener("submit", addItem);
+removeItems.addEventListener("submit", removeItem);
+clearAllBtn.addEventListener("click", clearAll);
 itemsList.addEventListener("click", toggleChecked);
